3ad53c3280a43596b72a128c3ef56b1f1e7057d2
akzholkoldoshov000 <akzholkoldoshov000>
2023-01-10 02:27:36 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Привет мир");
+        System.out.println(2005);
+        System.out.println(10+20+30);
+        System.out.println(10-3+6);
+        int mytown = 7;
+        int myhouse = 18;
+        double TODAYNUMBER =9;
+        System.out.println("Мой город" + mytown);
+        System.out.println("Мой дом" + myhouse);
+        System.out.println("сегодняшняя числло" + TODAYNUMBER);
+//Здесь хроняться только символы
+        char symbol = '-';
+        char symbol2 = ':';
+        char symbol3= '*';
+        char symbol4= '/';
+
+
+
+
+
+
+
+    }
+}
